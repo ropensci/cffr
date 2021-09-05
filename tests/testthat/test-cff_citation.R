1bf213d30c088039942b9e14a91a7a4f589dfef6
dieghernan <diego.hernangomezherrero@gmail.com>
2021-09-05 02:05:30 +0200

    Add tests

--- tests/testthat/test-cff_citation.R
+++ tests/testthat/test-cff_citation.R
@@ -18,3 +18,23 @@ test_that("Parse CITATION_ex", {
 
   expect_s3_class(parse_r_citation(desc_path, cit_path), "citation")
 })
+
+test_that("CITATION NULL", {
+  desc_path <- system.file("x", package = "cffr")
+  cit_path <- system.file("y", package = "cffr")
+
+  expect_null(parse_r_citation(desc_path, 
+    cit_path))
+    
+})
+
+
+test_that("Parse CITATION with no encoding", {
+  desc_path <- system.file("examples/DESCRIPTION_no_encoding", package = "cffr")
+  cit_path <- system.file("examples/CITATION_ex", package = "cffr")
+
+  s <- expect_warning(parse_r_citation(desc_path, 
+    cit_path))
+    
+    expect_s3_class(s, "citation")
+})
