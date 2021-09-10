cf204e3cef43ba6a962b4df359ddfa40a1921450
dieghernan <diego.hernangomezherrero@gmail.com>
2021-09-10 22:35:18 +0200

    Rerun tests

--- dev/testlocal.R
+++ dev/testlocal.R
@@ -5,20 +5,6 @@ setwd("C:/Users/diego/Documents/R/Projects/cffr")
 # Load package
 devtools::load_all()
 
-# Write table of packages
-installedall <- installed.packages()[, c("Package", "Version")]
-
-rownames(installedall) <- 1:nrow(installedall)
-
-
-writeLines(knitr::kable(installedall,
-                        caption = "Installed packages on this session",
-                        row.names = TRUE),
-           con = file.path(
-             "tests", "testthat", "test_local_installation",
-             "allpackages.md")
-)
-
 # Delete first the snaps folder
 unlink("./tests/testthat/test_local_installation/_snaps",
        recursive = TRUE
---
+++ tests/testthat/test_local_installation/test-full_cff.R
@@ -0,0 +1,111 @@
+test_that("Test ALL installed packages", {
+  expect_snapshot_output({
+    # skip("Skip by now") # by now
+
+    print_snapshot("Sessioninfo", sessionInfo())
+
+    installed <- as.data.frame(installed.packages()[, c("Package", "Version")])
+    installed <- installed[order(installed$Package), ]
+    l <- nrow(installed)
+
+    if (interactive()) {
+      size <- 1000
+      if (l > size) {
+        s <- sample(seq_len(l), size)
+        installed <- installed[s, ]
+      }
+    }
+
+
+    print_snapshot("Summary", paste(
+      "testing a sample of",
+      nrow(installed), "installed packages"
+    ))
+
+
+
+
+    res <- c()
+    withcit <- c()
+
+
+
+    for (i in c(1:nrow(installed))) {
+      message(installed[i, ]$Package)
+      desc_path <- file.path(find.package(installed[i, ]$Package), "DESCRIPTION")
+      cit_path <- file.path(find.package(installed[i, ]$Package), "CITATION")
+
+
+      if (file.exists(cit_path)) {
+        message("with CITATION file")
+        withcit <- c(withcit, TRUE)
+        citobj <- parse_r_citation(desc_path, cit_path)
+        citobj <- lapply(citobj, cff_parse_citation)
+        if (length(citobj) == 0) citobj <- NULL
+      } else {
+        citobj <- NULL
+        withcit <- c(withcit, FALSE)
+      }
+
+      # Add cffobj
+      cffobj <- cff_description(desc_path)
+
+      cffobj$doi <- clean_str(citobj[[1]]$doi)
+      cffobjend <- c(cffobj,
+        "preferred-citation" = citobj[1],
+        references = list(citobj[-1])
+      )
+
+
+
+      # Merge identifiers
+      oldids <- cffobjend$identifiers
+      cffobjend$identifiers <- c(
+        citobj[[1]]$identifiers,
+        oldids
+      )
+
+      # Order
+      cffobjend <- cffobjend[cff_schema_keys()]
+
+      cffobjend <- as.cff(cffobjend)
+      cffobjend
+
+      s <- suppressMessages(cff_validate(cffobjend))
+
+      res <- c(res, s)
+
+      if (!s) {
+        print_snapshot(paste("Errors on", installed[i]), cff_validate(cffobjend))
+      }
+    }
+
+
+
+    installed$with_citation <- withcit
+    installed$is_ok <- res
+
+
+    print_snapshot(
+      "Errors",
+      installed[installed$is_ok == FALSE, ]
+    )
+
+    print_snapshot(
+      paste("Packages processed:", nrow(installed)),
+      installed
+    )
+  })
+  message(getwd())
+
+  if (!file.exists("allpackages.md")) file.create("allpackages.md")
+
+  rownames(installed) <- 1:nrow(installed)
+
+  writeLines(knitr::kable(installed,
+    caption = "Tested packages on this session",
+    row.names = TRUE
+  ),
+  con = "allpackages.md"
+  )
+})
